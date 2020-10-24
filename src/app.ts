a46b8006d801d3c06f15e5d860e20b824361a360
Duduling <duduling20@gmail.com>
2020-10-24 16:54:53 +0900

    #2.65 DriversSubscription part One

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import cors from 'cors'
 import { NextFunction, Response } from 'express'
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 import helmet from 'helmet'
 import logger from 'morgan'
 import schema from './schema'
@@ -8,10 +8,26 @@ import decodeJWT from './utils/decodeJWT'
 
 class App {
 	public app: GraphQLServer
+	public pubSub: any
+	constructor() {
+		this.pubSub = new PubSub()
+		this.pubSub.ee.setMaxListeners(99)
+		this.app = new GraphQLServer({
+			schema,
+			context: (req) => {
+				return {
+					req: req.request,
+					pubSub: this.pubSub,
+				}
+			},
+		})
+		this.middlewares()
+	}
+
 	private middlewares = (): void => {
 		this.app.express.use(cors())
-		this.app.express.use(helmet())
 		this.app.express.use(logger('dev'))
+		this.app.express.use(helmet())
 		this.app.express.use(this.jwt)
 	}
 
@@ -23,7 +39,6 @@ class App {
 		const token = req.get('X-JWT')
 		if (token) {
 			const user = await decodeJWT(token)
-			console.log(user)
 			if (user) {
 				req.user = user
 			} else {
@@ -31,13 +46,6 @@ class App {
 			}
 		}
 		next()
-	}
-
-	constructor() {
-		this.app = new GraphQLServer({
-			schema,
-		})
-		this.middlewares()
 	}
 }
 
